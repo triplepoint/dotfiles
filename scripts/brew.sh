a349cb1dd69665254c092a2c453b1ebba91ddace
Jonathan Hanson <jonathan.hanson@planet.com>
2015-09-08 11:42:21 -0700

    Added more cask applications

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -11,7 +11,7 @@ brew update
 brew upgrade --all
 
 # Install cask (to manage OS X application installs)
-brew install brew-cask
+brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
@@ -22,18 +22,24 @@ brew install git
 brew install python
 brew install ssh-copy-id
 brew cask install sublime-text3
+brew cask install sourcetree
 brew install tree
-brew cask install vagrant
+brew cask install vagrant # Also installs Virtualbox
+#brew cask install totalterminal #test
 
 
 # General desktop-y stuff
 brew cask install adium
 brew install bash-completion
 brew cask install deluge
-#brew cask install dropbox # haven't tested, but should work
+#brew cask install dropbox # test
+#brew cask install firefox # test
+#brew cask install flux #test
 brew cask install google-chrome
 brew install gnupg
 brew cask install keepassx
+brew cask install karabiner
+
 
 # Clean up caches and such
 brew cask cleanup
