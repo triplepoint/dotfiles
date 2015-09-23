8ccb03cf0c9c81682ff95a2a59dcceb956ab7b31
Jonathan Hanson <jonathan@jonathan-hanson.org>
2015-09-22 20:33:12 -0700

    add some more brew casks

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -21,11 +21,12 @@ brew install      ansible
 brew cask install dockertoolbox
 brew install      git
 brew install      python
+#brew cask install pycharm-ce
 brew install      ssh-copy-id
 brew cask install sublime-text3
 brew cask install sourcetree
 brew install      tree
-brew cask install vagrant # Also installs Virtualbox
+brew cask install vagrant  # Also installs Virtualbox
 brew cask install totalterminal
 
 
@@ -33,15 +34,22 @@ brew cask install totalterminal
 brew cask install adium
 brew install      bash-completion
 brew cask install deluge
-#brew cask install dropbox # test
-#brew cask install firefox # test
-#brew cask install flux #test
+brew cask install dropbox
+brew cask install crashplan
+brew cask install firefox
+brew cask install flux
 brew cask install google-chrome
 brew install      gnupg
-brew cask install keepassx
+brew cask install keepassx0
 brew cask install karabiner
 brew install      nmap
 brew install      zsh
+
+
+# Other stuff
+brew cask install steam
+brew cask install spotify
+brew cask install github-desktop
 
 
 # Clean up caches and such
