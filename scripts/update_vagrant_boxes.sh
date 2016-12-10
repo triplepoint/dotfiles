2b3e108103d539fb61889aabba204bd0772d08d3
Jonathan Hanson <jonathan@jonathan-hanson.org>
2016-12-10 14:46:53 -0800

    add some comments.  Make the subl editor command for git have explicit long-form flags

--- scripts/update_vagrant_boxes.sh
+++ scripts/update_vagrant_boxes.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# List all the outdated vagrant boxes, and do an upgrade for each one
 vagrant box outdated --global 2>/dev/null | grep outdated | tr -d "*'" | cut -d ' ' -f 2 | xargs -I {} vagrant box add --clean {}
