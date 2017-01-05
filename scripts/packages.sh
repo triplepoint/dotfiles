0e322f06b169da2b96eb21ec5d523dafa95bf644
Jonathan Hanson <jonathan.hanson@planet.com>
2017-01-05 14:04:15 -0800

    remove some cruft, remove tapping alternate taps in favor if explcit 'paths'.  Add some command line replacements to the macos defaults.

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -15,15 +15,12 @@ vagrant_plugin_install () {
 
 # Stop on any error, print all commands
 set -ev
-# Install and/or update a collection of useful OSX packages
 
+# Install and/or update a collection of useful OSX packages
 ## Repository management
 ### Install brew taps
 brew tap caskroom/cask
 brew tap caskroom/fonts
-brew tap caskroom/versions
-brew tap homebrew/dupes
-brew tap homebrew/versions
 
 ### Update known packages
 brew update
@@ -33,59 +30,11 @@ brew upgrade
 
 
 ## Install/Update Packages
-### Languages and Language Tools
-brew install      go --cross-compile-common
-brew install      groovy
-brew cask install java
-brew install      python
-pip install       pip setuptools wheel --upgrade
-brew install      ruby
-brew install      rust
-brew install      scala
-brew install      sbt
-
-### General Programming Stuff
-brew install      ansible
-brew cask install atom
-pip install       awscli --upgrade
-pip install       boto3 --upgrade
-pip install       docker-py --upgrade
-brew cask install docker
-brew install      git
-brew install      git-annex
-brew cask install github-desktop
-brew install      gnu-sed --with-default-names
-brew install      homebrew/dupes/grep --with-default-names
-brew install      htop
-gem install       jekyll
-brew install      openssl
-brew cask install pgadmin3
-brew install      postgresql
-brew install      ssh-copy-id
-brew cask install sublime-text
-brew cask install sourcetree
-brew install      terraform
-brew install      tree
-brew install      unison
-brew install      vim
-brew cask install virtualbox
-brew cask install virtualbox-extension-pack
-brew cask install vagrant
-vagrant_plugin_install vagrant-vbguest
-vagrant_plugin_install vagrant-aws
-vagrant plugin update
-brew install      vault
-brew install      watch
-
-### Hardware development stuff
-brew cask install arduino
-brew cask install openscad
-brew cask install kicad
-
-### General desktop stuff
+### General Computer Stuff
 brew cask install android-file-transfer
 brew install      ansiweather
 brew cask install adium
+brew install      homebrew/dupes/awk
 brew install      bash
 brew cask install crashplan
 brew cask install deluge
@@ -97,23 +46,72 @@ brew cask install font-inconsolata-dz
 brew cask install functionflip # To swap function keys on non-apple keyboards
 brew cask install google-chrome
 brew install      gnupg2
-brew cask install iterm2-beta
+brew install      gnu-sed --with-default-names
+brew install      homebrew/dupes/grep --with-default-names
+brew install      htop
+brew cask install iterm2
 brew cask install keepassx
 brew cask install karabiner # To try to handle the ctrl/alt/option windows keyboard issue
 brew install      nmap
+brew install      openssl
+brew install      openssh
+brew install      homebrew/dupes/rsync
 brew cask install slack
+brew install      ssh-copy-id
+brew install      tree
+brew install      vim
+brew install      homebrew/dupes/whois
 brew install      zsh
 
-### "Other stuff"
+### Languages and Language Tools
+brew install      go --cross-compile-common
+brew install      groovy
+brew cask install java
+brew install      python
+pip install       pip setuptools wheel --upgrade
+brew install      ruby
+brew install      rust
+brew install      scala
+brew install      sbt
+
+### Programming and Ops Stuff
+brew install      ansible
+brew cask install atom
+pip install       awscli --upgrade
+pip install       boto3 --upgrade
+pip install       docker-py --upgrade
+brew cask install docker
+brew install      git
+brew install      git-annex
+brew cask install github-desktop
+gem install       jekyll
+brew cask install pgadmin3
+brew install      postgresql
+brew cask install sublime-text
+brew cask install sourcetree
+brew install      terraform
+brew install      unison
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+brew cask install vagrant
+vagrant_plugin_install vagrant-vbguest
+vagrant_plugin_install vagrant-aws
+vagrant plugin update
+brew install      vault
+brew install      watch
+
+### Hardware Development Stuff
+brew cask install arduino
+brew cask install openscad
+brew cask install kicad
+
+### Recreational Stuff
 brew cask install steam
 brew cask install battle-net
 brew install      libao --HEAD  # for pianobar
 brew install      pianobar
 brew cask install spotify
 brew cask install xscreensaver
-
-## Pin specific versions (often temporary)
-# brew unlink ansible && brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/54bf24fbe302ad27f0eba4d6f7914d6a8ccc876d/Formula/ansible.rb && brew switch ansible 2.1.1.0 && brew pin ansible
 
 ## Clean up package caches and such
 brew cleanup
