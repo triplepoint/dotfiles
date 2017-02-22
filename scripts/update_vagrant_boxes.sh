d2e7dd5354c5d8473e29b77c4af26fa1517542c0
Jonathan Hanson <jonathan.hanson@planet.com>
2017-02-21 17:41:11 -0800

    parallelize the vagrant box update script

--- scripts/update_vagrant_boxes.sh
+++ scripts/update_vagrant_boxes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # List all the outdated vagrant boxes, and do an upgrade for each one
-vagrant box outdated --global 2>/dev/null | grep outdated | tr -d "*'" | cut -d ' ' -f 2 | xargs -I {} vagrant box add --clean {}
+vagrant box outdated --global 2>/dev/null | grep outdated | tr -d "*'" | cut -d ' ' -f 2 | xargs -n 1 -P 8 -I {} vagrant box add --clean {}
