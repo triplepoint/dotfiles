badfee8f224ad95adb7b39d2f58eaba9257408f3
Jonathan Hanson <jonathan.hanson@planet.com>
2017-03-28 14:20:52 -0700

    allow errors during uninstalling things

--- scripts/clean_old_packages.sh
+++ scripts/clean_old_packages.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# Stop on any error, print all commands
-set -ev
-
-brew cask zap     flux    # MacOS added their own feature
-brew uninstall    ansible # handling this with pip instead
-brew cask zap     functionflip  # My current keyboards don't need this
-pip uninstall     docker-py  # Replaced with 'docker'
-brew cask zap     github-desktop  # Never used this
+brew cask zap     flux              || true # MacOS added their own feature
+brew uninstall    ansible           || true # handling this with pip instead
+brew cask zap     functionflip      || true # My current keyboards don't need this
+pip uninstall     docker-py         || true # Replaced with 'docker'
+brew cask zap     github-desktop    || true # Never used this
