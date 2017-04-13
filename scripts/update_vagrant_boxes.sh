c593a5c77d20a01ecb2bcfb5b268596e80e658f2
Jonathan Hanson <jonathan.hanson@planet.com>
2017-04-13 15:26:45 -0700

    also prune vagrant boxes after updating

--- scripts/update_vagrant_boxes.sh
+++ scripts/update_vagrant_boxes.sh
@@ -2,3 +2,6 @@
 
 # List all the outdated vagrant boxes, and do an upgrade for each one
 vagrant box outdated --global 2>/dev/null | grep outdated | tr -d "*'" | cut -d ' ' -f 2 | xargs -I {} vagrant box add --clean {}
+
+# Prune out any old versions
+vagrant box prune
