fe637b7560ffe91fb7edf79a76ef1e974d2e4335
Jonathan Hanson <jonathan.hanson@planet.com>
2017-11-29 19:56:12 -0800

    set up a global git ignore file

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -7,5 +7,6 @@ git config --global user.name "Your Name"
 git config --global user.email "email@example.com"
 
 git config --global core.editor "atom --new-window --wait"
+git config --global core.excludesfile ~/.gitignore_global
 git config --global diff.compactionHeuristic true
 git config --global rerere.enabled true
