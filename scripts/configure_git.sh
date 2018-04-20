a8d0f4132ca762b234cdedf51be75ed03ebf7ad5
Jonathan Hanson <jonathan@jonathan-hanson.org>
2018-04-20 13:49:42 -0700

    add a ansible vault view diff

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -11,3 +11,9 @@ git config --global core.editor "atom --new-window --wait"
 git config --global core.excludesfile ~/.gitignore_global
 git config --global diff.compactionHeuristic true
 git config --global rerere.enabled true
+
+# Ansible Vault diffs
+# will need a .gitattributes file in the respective repos with something like:
+#   inventories/**/*vault*.yml diff=ansible-vault merge=binary
+git config --global diff.ansible-vault.textconv "ansible-vault view"
+git config --global diff.ansible-vault.cachetextconv false
