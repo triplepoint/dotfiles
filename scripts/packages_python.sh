2b201b07205a9a1d303d8f2d43116766c054e81b
Jonathan Hanson <jonathan@jonathan-hanson.org>
2018-07-01 15:59:46 -0700

    turns out I don't need to delete existing venvs first (which was failing on the strict set -e case)

--- scripts/packages_python.sh
+++ scripts/packages_python.sh
@@ -6,11 +6,9 @@ pip3 install pip setuptools wheel virtualenv --upgrade
 
 pushd "${BASH_SOURCE%/*}"
 
-### Generate the package manifests for Python 2 and 3
+### Generate the complete package manifests for Python 2 and 3
 PIPENV_PIPFILE=Pipfile.py2 pipenv lock --two -r > requirements_py2.txt
-pipenv --rm
 PIPENV_PIPFILE=Pipfile.py3 pipenv lock --three -r > requirements_py3.txt
-pipenv --rm
 
 # ### Install Packages for the Python 2 and 3 environments
 pip2 install -r requirements_py2.txt
