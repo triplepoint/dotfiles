1ea4a4940fe641ae8ac1c83602a00dd05b2af7bc
Jonathan Hanson <jonathan.hanson@planet.com>
2018-07-17 16:31:57 -0700

    on second thought, deal with the error rather than suppress it

--- scripts/update_vagrant_boxes.sh
+++ scripts/update_vagrant_boxes.sh
@@ -4,7 +4,7 @@
 set -v
 
 # List all the outdated vagrant boxes, and do an upgrade for each one
-vagrant box outdated --global 2>/dev/null | grep outdated | tr -d "*'" | cut -d ' ' -f 2 | xargs -I {} vagrant box add --clean {}
+vagrant box outdated --global 2>/dev/null | grep outdated | tr -d "*'" | cut -d ' ' -f 2 | xargs -I {} vagrant box add --clean --force {}
 
 # Prune out any old versions
 vagrant box prune
