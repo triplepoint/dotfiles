9d07ee2b44faa996d53e431996f03fdf147865c5
Jonathan Hanson <jonathan@jonathan-hanson.org>
2018-08-02 14:48:52 -0700

    Reorganize the package list a bit

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -34,29 +34,40 @@ brew upgrade
 ## Install/Update Packages
 ### General Computer Stuff
 brew cask install android-file-transfer
-brew install      gawk
-brew install      bash
-brew cask install deluge
-brew cask install dropbox
-brew cask install firefox
 brew cask install caskroom/fonts/font-inconsolata
 brew cask install caskroom/fonts/font-inconsolata-dz
+brew cask install deluge
 brew cask install discord
+brew cask install dropbox
+brew cask install firefox
 brew cask install gimp
 brew cask install google-chrome
-brew install      gnupg2
-brew install      gnu-sed --with-default-names
-brew install      grep --with-default-names
-# brew install      htop  Temporarily disabled due to high sierra incompatiblity
-brew cask install iterm2
-brew install      iperf3
 brew cask install keepassxc
-brew install      mtr
-brew install      md5sha1sum
-brew install      nmap
 brew install      openssl
-brew install      rsync
 brew cask install slack
+brew cask install xscreensaver
+
+### Recreational Stuff
+brew cask install steam
+brew cask install battle-net
+brew install      libao --HEAD  # for pianobar
+brew install      pianobar
+brew cask install spotify
+brew cask install vox
+
+### Terminals, Shells, and Command Line Utilities
+brew install      bash
+brew install      gawk
+brew install      gnu-sed --with-default-names
+brew install      gnupg2
+brew install      grep --with-default-names
+brew install      iperf3
+brew cask install iterm2
+brew install      md5sha1sum
+brew install      mtr
+brew install      nmap
+brew install      rename
+brew install      rsync
 brew install      ssh-copy-id
 brew install      tree
 brew install      vim
@@ -66,23 +77,22 @@ brew install      zsh-completions
 
 ### Languages and Language Tools
 brew install      go --cross-compile-common
+brew cask install java       # Should be installed before JVM languages like scala and groovy
 brew install      groovy
-brew cask install java
 brew install      php
 brew install      python     # Installs Python 3
 brew install      python@2   # Installs Python 2
-brew install      ruby
 brew install      scala
-brew install      sbt
 source "${BASH_SOURCE%/*}/python/_packages_python.sh"
+brew install      ruby
 source "${BASH_SOURCE%/*}/_packages_rust.sh"
+brew install      sbt        # Tooling for scala, will source Scala itself
 
 ### Programming and Ops Stuff
 brew cask install atom
-apm install       sync-settings
+apm install       sync-settings    # Atom plugin, for storing settings in github gists
 brew cask install docker
 brew install      git
-# brew install      git-annex
 brew install      git-lfs
 gem install       jekyll
 brew install      postgresql
@@ -104,24 +114,19 @@ brew cask install arduino
 brew cask install kicad
 brew cask install meshmixer
 
-### Recreational Stuff
-brew cask install steam
-brew cask install battle-net
-brew install      libao --HEAD  # for pianobar
-brew install      pianobar
-brew cask install spotify
-brew cask install vox
-brew cask install xscreensaver
 
 ## Clean up package caches and such
 brew cleanup
 brew cask cleanup
 
-## Any pinned brew packages, as a reminder to review them:
-brew list --pinned || true
 
 ## Anything wrong with brew?
 brew doctor        || true
+
+
+## Any pinned brew packages, as a reminder to review them:
+brew list --pinned || true
+
 
 ## Outdated cask packages (not super reliable):
 brew cask outdated || true
