5fdb9441128f8c775d2ceace37583f0745c6efd5
Jonathan Hanson <jonathan@jonathan-hanson.org>
2018-10-07 23:35:50 -0700

    Parameterize the python version, so we know the package manifests are built with the same versions of python as the global versions

--- scripts/python/_packages_python.sh
+++ scripts/python/_packages_python.sh
@@ -4,18 +4,22 @@
 set -e
 
 ### xcode's libraries need to be shown to command-line compilers
-### Note that this is also done in the .zshrc file, but are here in case that
+### NOTE: this is also done in the .zshrc file, but are here in case that
 ### file hasn't yet been installed
 export CFLAGS="-I$(xcrun --show-sdk-path)/usr/include"
 
+### Define the versions of python to use as the global 2 and 3 versions
+GLOBAL_PY2="2.7.15"
+GLOBAL_PY3="3.7.0"
+
 ### Ensure python 2 and 3 are installed, and set as global
 brew install pyenv
-pyenv install -s 2.7.15
-pyenv install -s 3.7.0
-pyenv global 3.7.0 2.7.15
+pyenv install -s ${GLOBAL_PY2}
+pyenv install -s ${GLOBAL_PY3}
+pyenv global ${GLOBAL_PY3} ${GLOBAL_PY2}
 
 ### Enable pyenv
-### Note that these are also done in the .zshrc file, but are here in case that
+### NOTE: these are also done in the .zshrc file, but are here in case that
 ### file hasn't yet been installed
 export PATH="$HOME/.pyenv:$PATH"
 eval "$(pyenv init -)"
@@ -28,10 +32,10 @@ pushd "${BASH_SOURCE%/*}"
 
 ### Generate the complete package manifests for Python 3 and 2
 rm -f Pipfile.py*.lock
-PIPENV_PIPFILE=Pipfile.py3 pipenv lock --three -r > requirements_py3.txt
-PIPENV_PIPFILE=Pipfile.py2 pipenv lock --two -r > requirements_py2.txt
+PIPENV_PIPFILE=Pipfile.py3 pipenv lock -v --python ${GLOBAL_PY3} -r > requirements_py3.txt
+PIPENV_PIPFILE=Pipfile.py2 pipenv lock -v --python ${GLOBAL_PY2} -r > requirements_py2.txt
 
-# ### Install Packages for the Python 3 and 2 environments
+### Install Packages for the Python 3 and 2 environments
 pip3 install -r requirements_py3.txt
 pip2 install -r requirements_py2.txt
 
