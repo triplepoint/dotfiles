7e543323df4108918eb3a72736d8f485200da998
Jonathan Hanson <jhanson@ellation.com>
2018-10-30 11:44:21 -0700

    space out the python stuff more, so its easier to see where the different stages are

--- scripts/python/_packages_python.sh
+++ scripts/python/_packages_python.sh
@@ -15,8 +15,11 @@ GLOBAL_PY3="3.7.0"
 
 ### Ensure python 2 and 3 are installed, and set as global
 brew install pyenv
+
 pyenv install -s ${GLOBAL_PY2}
+
 pyenv install -s ${GLOBAL_PY3}
+
 pyenv global ${GLOBAL_PY3} ${GLOBAL_PY2}
 
 ### Enable pyenv
@@ -28,18 +31,28 @@ eval "$(pyenv init -)"
 set -v
 
 ### System level Python package management tools
+#### Python3 support packages
 pip3 install pip setuptools wheel virtualenv pipenv --upgrade
+
+#### Python2 support packages
 pip2 install pip setuptools wheel virtualenv --upgrade
 
 pushd "${BASH_SOURCE%/*}"
 
 ### Generate the complete package manifests for Python 3 and 2
-rm -f Pipfile.py*.lock
+#### Generate Python 3 intermediate requirements.txt file
+rm -f Pipfile.py3.lock
 PIPENV_PIPFILE=Pipfile.py3 pipenv lock --python ${GLOBAL_PY3} -r > requirements_py3.txt
+
+#### Generate Python 2 intermediate requirements.txt file
+rm -f Pipfile.py2.lock
 PIPENV_PIPFILE=Pipfile.py2 pipenv lock --python ${GLOBAL_PY2} -r > requirements_py2.txt
 
 ### Install Packages for the Python 3 and 2 environments
+#### Install Python 3 packages
 pip3 install -r requirements_py3.txt
+
+#### Install Python 2 packages
 pip2 install -r requirements_py2.txt
 
 popd
