9aa94ef0068e1df800fe0b026e0dad01934ed96c
Jonathan Hanson <jonathan@jonathan-hanson.org>
2020-05-18 13:31:25 -0700

    don't install prusa-slicer with apt

--- scripts/linux/packages.sh
+++ scripts/linux/packages.sh
@@ -99,4 +99,4 @@ sudo snap install        freecad
 sudo apt-get install -y  kicad
 # brew_cask_install meshmixer
 sudo apt-get install -y  openscad
-sudo apt-get install -y  prusa-slicer
+# sudo apt-get install -y  prusa-slicer  # This was out of date.  Use the .appimage from the https://www.prusa3d.com/drivers/ site instead
