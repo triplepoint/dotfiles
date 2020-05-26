a36b1f02ce8c9cf185158bb9bec38a6469be35c2
Jonathan Hanson <jonathan @jonathan-hanson.org>
2020-05-26 14:58:25 -0700

    some of the linux shell scripts didn't like ~ as a reference.  Used /home/jhanson instead.

--- scripts/macos/copy_dotfiles.sh
+++ scripts/macos/copy_dotfiles.sh
@@ -10,16 +10,16 @@ GLOBAL_DF_HOME_PATH=$(dirname $0)/../../home/global
 OS_DF_HOME_PATH=$(dirname $0)/../../home/macos
 
 ## Copy Over Standard Environment Files
-cp -fp $GLOBAL_DF_HOME_PATH/.gitignore_global ~
-cp -fp $GLOBAL_DF_HOME_PATH/.gitconfig ~
-cp -fp $GLOBAL_DF_HOME_PATH/.vimrc ~
-cp -fp $OS_DF_HOME_PATH/.zshrc ~
+cp -fp $GLOBAL_DF_HOME_PATH/.gitignore_global ${HOME}
+cp -fp $GLOBAL_DF_HOME_PATH/.gitconfig ${HOME}
+cp -fp $GLOBAL_DF_HOME_PATH/.vimrc ${HOME}
+cp -fp $OS_DF_HOME_PATH/.zshrc ${HOME}
 
-mkdir -p ~/.vagrant.d
-cp -fp $GLOBAL_DF_HOME_PATH/.vagrant.d/Vagrantfile ~/.vagrant.d/
+mkdir -p ${HOME}/.vagrant.d
+cp -fp $GLOBAL_DF_HOME_PATH/.vagrant.d/Vagrantfile ${HOME}/.vagrant.d/
 
 ## Execution Path
 ### Useful command-line utilities (if present)
-mkdir -p ~/bin
-cp -fp $GLOBAL_DF_HOME_PATH/bin/* ~/bin || true
-cp -fp $OS_DF_HOME_PATH/bin/* ~/bin || true
+mkdir -p ${HOME}/bin
+cp -fp $GLOBAL_DF_HOME_PATH/bin/* ${HOME}/bin || true
+cp -fp $OS_DF_HOME_PATH/bin/* ${HOME}/bin || true
