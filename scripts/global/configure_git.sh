78b8ba1db3df0db66310f31d765bd3e5dce5b537
Jonathan Hanson <jonathan@jonathan-hanson.org>
2020-09-06 14:25:56 -0700

    make new git repos have 'main' as their default branch.  Switch to histogram diff algorithm.

--- scripts/global/configure_git.sh
+++ scripts/global/configure_git.sh
@@ -8,6 +8,13 @@ set -ev
 # git config --global user.email "email@example.com"
 
 git config --global core.editor vim
+
 git config --global core.excludesfile ~/.gitignore_global
+
+git config --global diff.algorithm histogram
 git config --global diff.indentHeuristic true
+
 git config --global rerere.enabled true
+
+# Make 'main' the default branch name on new repositories
+git config --global init.defaultBranch main
