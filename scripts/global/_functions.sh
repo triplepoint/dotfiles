8ddc2d00fdc60a69f6bed1c0f766c3f3f4422826
Jonathan Hanson <jonathan@jonathan-hanson.org>
2020-11-07 17:21:20 -0800

    move the script-finding functions down into the global/_functions.sh

--- scripts/global/_functions.sh
+++ scripts/global/_functions.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTS_ROOTDIR=$(dirname ${THIS_DIR})
+
 # Dump the list of installed vagrant plugins to a cache file,
 # so we can speed up the "do we need to reinstall this"
 # process.  Note that the file gets written here, in the scripts/global/
@@ -27,4 +30,38 @@ vagrant_plugin_install () {
     vagrant plugin install $1
   fi
   set -v
+}
+
+# Determine which OS we're on, and return a string that's a decent shorthand description of that OS
+# We'll use this string as the directory name of the OS-specific scripts in this repo.
+which_os () {
+    UNAME_RESULT="$(uname -s)"
+    case "${UNAME_RESULT}" in
+        Linux*)     OS_GUESS=linux;;
+        Darwin*)    OS_GUESS=macos;;
+        CYGWIN*)    OS_GUESS=cygwin;;
+        MINGW*)     OS_GUESS=mingw;;
+        *)          OS_GUESS="UNKNOWN:${UNAME_RESULT}"
+    esac
+    echo ${OS_GUESS}
+}
+
+# Given a script name and any parameters for that script, attempt to find it in either the os-specific
+# directory, or the global directory, and then run it.
+run_script () {
+    SCRIPT_NAME=$1
+    shift
+
+    OSDIR=$(which_os)
+
+    if [ -f "${SCRIPTS_ROOTDIR}/${OSDIR}/${SCRIPT_NAME}" ]; then
+        echo "Executing: ${SCRIPTS_ROOTDIR}/${OSDIR}/${SCRIPT_NAME} $@"
+        ${SCRIPTS_ROOTDIR}/${OSDIR}/${SCRIPT_NAME} $@
+    elif [ -f "${SCRIPTS_ROOTDIR}/global/${SCRIPT_NAME}" ]; then
+        echo "Executing: ${SCRIPTS_ROOTDIR}/global/${SCRIPT_NAME} $@"
+        ${SCRIPTS_ROOTDIR}/global/${SCRIPT_NAME} $@
+    else
+        echo "Unknown script ${SCRIPT_NAME}"
+        exit 1
+    fi
 }
