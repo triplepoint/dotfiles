7b8e2ca5c6b2f3177d2428cf7a003bd808b80c2d
Jonathan Hanson <jonathan@exabyte.io>
2021-03-03 13:38:12 -0800

    this needs to be properly escaped

--- home/global/.oh-my-zsh/custom/aliases.zsh
+++ home/global/.oh-my-zsh/custom/aliases.zsh
@@ -1,1 +1,1 @@
-alias git_fetch_submodules="git fetch --all --prune --tags && git submodule status --recursive | cut -c2- | cut -d' ' -f2 | xargs -P4 -I% bash -c "echo --% && git -C % fetch --all --prune --tags""
+alias git_fetch_submodules="git fetch --all --prune --tags && git submodule status --recursive | cut -c2- | cut -d' ' -f2 | xargs -P8 -I% bash -c 'echo --% && git -C % fetch --all --prune --tags'"
