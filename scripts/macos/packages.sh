0d5cc3d3c6cea321e54b51d0e74887ce52e0b5c2
Jonathan Hanson <jonathan@jonathan-hanson.org>
2021-04-06 16:22:29 -0700

    add signal

--- scripts/macos/packages.sh
+++ scripts/macos/packages.sh
@@ -60,6 +60,7 @@ brew_cask_install keepassxc
 brew_cask_install libreoffice
 brew_install      openssl
 brew_cask_install rectangle
+brew_cask_install signal
 brew_cask_install skype
 brew_cask_install slack
 brew_install      tcl-tk
