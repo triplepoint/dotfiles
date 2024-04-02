4799c1fe545e1c3b60a9ecc8fc52bf63efc8f971
Jonathan Hanson <jonathan.hanson@saildrone.com>
2024-04-02 10:33:55 -0700

    review versions

--- scripts/linux/packages.sh
+++ scripts/linux/packages.sh
@@ -82,7 +82,7 @@ sudo apt install -q -y   keepassxc
 sudo apt install -q -y   signal-desktop
 
 # see: https://slack.com/intl/en-in/downloads/instructions/ubuntu, see the Try Again link
-SLACK_VER=4.36.140
+SLACK_VER=4.37.94
 install_deb              https://downloads.slack-edge.com/releases/linux/${SLACK_VER}/prod/x64/slack-desktop-${SLACK_VER}-amd64.deb slack
 
 sudo apt install -q -y   syncthing
@@ -152,7 +152,7 @@ sudo apt install -q -y   vault
 sudo apt install -q -y   code
 
 # see: https://obsidian.md/download
-OBSIDIAN_VER=1.5.11
+OBSIDIAN_VER=1.5.12
 install_app_image        https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VER}/Obsidian-${OBSIDIAN_VER}.AppImage
 
 
@@ -174,5 +174,5 @@ sudo apt install -q -y   openscad
 sudo apt install -q -y   picocom
 
 # see: https://help.prusa3d.com/downloads under MK2.5S
-PRUSA_VER=2_7_1
+PRUSA_VER=2_7_3
 install_app_image        https://cdn.prusa3d.com/downloads/drivers/prusa3d_linux_${PRUSA_VER}.zip
