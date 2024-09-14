0671cded88569a2729b2072f648a70a16f020fdb
Jonathan Hanson <jonathan@jonathan-hanson.org>
2024-09-13 20:16:46 -0700

    bump python

--- scripts/global/python/_packages_python3.sh
+++ scripts/global/python/_packages_python3.sh
@@ -11,7 +11,7 @@ set -e
 
 ### Define the versions of python to use as the global versions (space separated)
 # see: https://www.python.org/downloads/
-GLOBAL_PY="3.12.5"
+GLOBAL_PY="3.12.6"
 
 ### Define the Python that we'll consider the dominant one
 PRIMARY_PY=$(echo ${GLOBAL_PY} | cut -d ' ' -f1)
